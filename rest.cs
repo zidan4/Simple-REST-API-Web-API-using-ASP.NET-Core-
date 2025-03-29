872636648cc55dc169349bf1852ff8fd5b9afcf7
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-03-29 17:04:41 +0300

    Create rest.cs

---
+++ rest.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/hello", () => new { message = "Hello, World!" });
+
+app.Run();
